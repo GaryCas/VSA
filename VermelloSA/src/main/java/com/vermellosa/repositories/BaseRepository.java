9f4efe10658e502e22d1431cd97334f19b54bfc5
Garreth Cassar <garreth.cassar@appsbroker.com>
2016-08-20 12:38:06 +0100

    Base repository initialized with QueryBuilder

--- VermelloSA/src/main/java/com/vermellosa/repositories/BaseRepository.java
+++ VermelloSA/src/main/java/com/vermellosa/repositories/BaseRepository.java
@@ -1,6 +1,10 @@
 package com.vermellosa.repositories;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+
 import java.util.List;
+import java.util.Map;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
@@ -9,13 +13,56 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
  */
 public abstract class BaseRepository<T> {
 
-    public List<T> findAll() {
-        return ofy().load().type(getType()).list();
+    protected Query<T> getBaseLoadQuery(){
+        return ofy().load().type(getType());
     }
 
-    public void save(T entity){
-        ofy().save().entity(entity).now();
+    public Key<T> save(T entity){
+        return ofy().save().entity(entity).now();
+    }
+
+    public Map<Key<T>, T> save(List<T> entityList){
+        return ofy().save().entities(entityList).now();
+    }
+
+    public void delete(T entity) {
+        ofy().delete().entity(entity).now();
+    }
+
+    public void delete(List<T> entities) {
+        ofy().delete().entities(entities).now();
+    }
+
+
+    public List<T> findAll() {
+        return getBaseLoadQuery().list();
     }
 
     public abstract Class<T> getType();
+
+    protected class QueryBuilder{
+        private Query<T> target;
+        boolean targetInitialised = false;
+
+        public QueryBuilder(){
+            initTarget();
+        }
+
+        private void initTarget() {
+            target = getBaseLoadQuery();
+            targetInitialised = true;
+        }
+
+        public QueryBuilder withFilter(String filter, Object value){
+            if(value != null && targetInitialised){
+                target.filter(filter, value);
+            }
+            return this;
+        }
+
+        public Query<T> build(){
+            return target;
+        }
+
+    }
 }
