ba390a13cce71249363f0d36c886f2afb427c4ac
Garreth Cassar <garreth.cassar@appsbroker.com>
2016-09-02 14:02:56 +0100

    Auth set up Watch out for DI set up

--- src/main/java/com/vermellosa/di/MainChildModule.java
+++ src/main/java/com/vermellosa/di/MainChildModule.java
@@ -1,7 +1,11 @@
 package com.vermellosa.di;
 
+import com.google.appengine.api.appidentity.AppIdentityService;
+import com.google.appengine.api.appidentity.AppIdentityServiceFactory;
 import com.google.appengine.api.utils.SystemProperty;
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import com.googlecode.objectify.ObjectifyFilter;
 
 /**
  * Created by Gary Cassar on 22/08/2016.
@@ -10,7 +14,9 @@ public class MainChildModule extends AbstractModule{
 
     @Override
     protected void configure() {
+        bind(ObjectifyFilter.class).in(Singleton.class);
 
+        bind(AppIdentityService.class).toInstance(AppIdentityServiceFactory.getAppIdentityService());
     }
 
 
